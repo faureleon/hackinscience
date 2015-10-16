04831fae45f0e29d8e440c6fc3cf45ebf9b57f68
Léon Faure <leon.faure@cri-paris.org>
2015-10-16 16:53:30 +0200

    rt

--- exercises/008/solution.py
+++ exercises/008/solution.py
@@ -1,4 +1,3 @@
-T=["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d", " ", "!"]
-for i in range (13):
-	print(T[i])
-
+T = ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d", " ", "!"]
+for i in range(13):
+    print(T[i])
