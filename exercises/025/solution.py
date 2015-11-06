6995711de337a7d4e1fa51001b5263dfa9ea60d5
Léon Faure <leon.faure@cri-paris.org>
2015-11-06 17:21:17 +0100

    savelast

--- exercises/025/solution.py
+++ exercises/025/solution.py
@@ -1,4 +1,4 @@
 import datetime
-A = datetime.datetime.now().date()
-B = datetime.datetime.now().strftime("%H:%M:%S")
-print("Today is"), A, ("and it is"), B
+A = (datetime.datetime.now().date())
+B = (datetime.datetime.now().strftime("%H:%M:%S"))
+print("Today is", A, "and it is", B)
