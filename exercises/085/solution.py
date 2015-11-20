9ee29c669d66e4b9bd7e694f6bdcd49a087346d7
Léon Faure <leon.faure@cri-paris.org>
2015-11-20 16:57:50 +0100

    save85.3

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -1,18 +1,11 @@
 def sort_a_list(l):
-    OK = False
-    while OK == False:
-        OK = True
-        for i in range(len(l) - 1):
-            if l[i] < l[i + 1]:
-                K = l[i]
-                l[i] = l[i + 1]
-                l[i + 1] = K
-                OK = False
-    return(l)
+    a = sorted(l, reverse=True)
+    return(a)
 def sort_by_mark(my_class):
-    sorted(my_class, reverse=True)
-    return(my_class)
+    a = sorted(my_class, reverse=True)
+    return(a)
 from operator import itemgetter
 def sort_by_name(my_class):
     getcount = itemgetter(1)
-    return(sorted(my_class, key=getcount))
+    a = sorted(my_class, key=getcount)
+    return(a)
