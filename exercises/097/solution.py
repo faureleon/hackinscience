71ce6f22207f43c4a725948a3fcfe23da05eaf02
Léon Faure <leon.faure@cri-paris.org>
2015-11-27 17:16:06 +0100

    save97bis

--- exercises/097/solution.py
+++ exercises/097/solution.py
@@ -5,12 +5,11 @@ def love_meet(bob, alice):
     return(b)
 
 
-def affair_meet(alice, bob, silvester):
+def affair_meet(bob, alice, sylvester):
     A = set(alice)
     B = set(bob)
     S = set(silvester)
-    b = set(A).intersection(B)
-    c = set(A).intersection(S)
-    d = [val for val in b if val not in c]
+    b = set(A).intersection(S)
+    d = [val for val in b if val not in B]
     D = set(d)
     return(D)
